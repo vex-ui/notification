3947235837831d470353d2c0d929ec106cf19b33
shareef <70265506+sherif414@users.noreply.github.com>
2023-11-30 16:37:56 +0200

    refactor(plugin): install the plugin as a separate app

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
-import { h, render } from 'vue'
-import type { VNode, Component } from 'vue'
-import NotificationRoot from './VNotificationRoot.vue'
 import { useID } from '@/composables'
+import { remove } from '@/utils'
+import type { Component, InjectionKey, Plugin, Ref } from 'vue'
+import { createApp, shallowRef } from 'vue'
+import NotificationRoot from './VNotificationRoot.vue'
 
 export interface NotifyOptions {
   body?: string | Component
@@ -17,28 +18,32 @@ export interface NotificationItem extends NotifyOptions {
   key: string
 }
 
-let Root: VNode | null = null
+export interface NotificationPluginOptions {}
 
-function useNotification() {
-  if (!Root) {
-    Root = h(NotificationRoot)
-    render(Root, document.createDocumentFragment() as unknown as Element)
-  }
+export interface AppContext extends NotificationPluginOptions {
+  notifications: Ref<NotificationItem[]>
+}
+
+export const APP_CONTEXT = Symbol() as InjectionKey<AppContext>
+
+export const NotificationPlugin: Plugin = (options: NotificationPluginOptions) => {
+  const app = createApp(NotificationRoot)
+  const notifications = shallowRef<NotificationItem[]>([])
+  app.provide(APP_CONTEXT, { ...options, notifications })
+  app.mount(document.appendChild(document.createElement('div')))
 
   return {
     notify: (options: NotifyOptions = {}) => {
       const key = useID()
       const notification = { key, ...options }
-      Root?.component?.exposed?.addNotification(notification)
+      notifications.value.push(notification)
       return {
         key,
-        remove: () => Root?.component?.exposed?.removeNotification(notification),
+        remove: () => remove(notifications.value, notification),
       }
     },
     clearAllNotification: () => {
-      Root?.component?.exposed?.clearAll()
+      notifications.value = []
     },
   }
 }
-
-export { useNotification }
