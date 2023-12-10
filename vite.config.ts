b6bfe7eccbd844d6a12a95ba39789b78db726cb6
shareef <70265506+sherif414@users.noreply.github.com>
2023-12-10 16:43:56 +0200

    removed unused dep

--- vite.config.ts
+++ vite.config.ts
@@ -23,12 +23,8 @@ export default defineConfig({
       formats: ['es'],
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ['vue'],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           vue: 'Vue',
         },
