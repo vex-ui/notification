4476ec79e9dff4a340086a5308b40defddf00959
shareef <70265506+sherif414@users.noreply.github.com>
2024-01-07 16:10:29 +0200

    skip

--- playground/main.ts
+++ playground/main.ts
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'uno.css'
 import '@unocss/reset/tailwind.css'
-import { VexToastPlugin } from '../src/plugin'
 
 const app = createApp(App)
-app.use(VexToastPlugin, {})
 app.mount('#app')
